156a82e8d258e0a528b18cefb3429a4945044ca9
Oliveira <boostygor@gmail.com>
2025-08-26 22:25:31 -0300

    v0.0.6 - criação de módulos pra ficar organizado

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
 import discord
+from discord.ext import commands
+import os
 
 # ------------------ /  T  /  -------------------
 
@@ -8,6 +9,10 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
+for filename in os.listdir("./modulos"):
+    if filename.endswith(".py"):
+        client.load_extension(f"modulos.{filename[:-3]}")
 
 @client.event
 async def on_ready():
