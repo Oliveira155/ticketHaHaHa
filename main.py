90cb4d4cf89c30e549dac33062a46da4f55e8b92
Oliveira <boostygor@gmail.com>
2025-08-26 23:32:22 -0300

    v0.0.8 - segurança no token, .gitignore adicionado

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
 import os
+from config import DISCORD_TOKEN
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = DISCORD_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
